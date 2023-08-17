a2cd92a586e6c3ebf2d9ca31a70e855727fa7c23
SanyaKontr <Bs13111@mail.ru>
2023-08-17 18:53:20 +0300

    Задания с 1 по 5

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /*let a=20;
-alert(a);*/
+alert(a);
 
 let name = 'Брендан Эйх';
 alert(name);
@@ -39,11 +39,86 @@ const user = {
 }
 user['age'] = 25;
 
-/*user['city of residence'];*/
+/*user['city of residence'];
 
 let info=prompt('Какую информацию хотите узнать о пользователе?')
 
 alert(user[info]);
 
 let nameUser= prompt('Ваше имя')
-alert('Привет, ' + nameUser+'!')
+alert('Привет, ' + nameUser+'!')*/
+
+/*let password = '12345';
+let passwordUser = prompt('Введите пароль');
+
+if (password === passwordUser) {
+   alert('Пароль введен верно') 
+} else {
+    alert('Пароль введен не верно') 
+}
+
+let c = 7;
+if (c > 0 && c < 10) {
+    console.log('Верно');
+} else {
+    console.log('Не верно');
+}
+
+let d = 150;
+let e = 25;
+
+if (d > 100 || e > 100) {
+    console.log('Верно');
+} else {
+    console.log('Не верно');
+}
+
+
+let a = '2';
+let b = '3';
+// Код выше изменять менять нельзя, чтобы решить задачу исправьте код ниже: 
+alert(Number(a) + Number(b));*/
+
+
+let month = String(prompt('Какой сейчас номер месяца?'))
+switch (month) {
+    case '1':
+        console.log('Зима');
+        break;
+    case '2':
+        console.log('Зима');
+        break;
+    case '3':
+        console.log('Весна');
+        break;
+    case '4':
+        console.log('Весна');
+        break;
+    case '5':
+        console.log('Весна');;
+        break;
+    case '6':
+        console.log('Лето');
+        break;
+    case '7':
+        console.log('Лето');
+        break;
+    case '8':
+        console.log('Лето');
+        break;
+    case '9':
+        console.log('Осень');
+        break;
+    case '10':
+        console.log('Осень');
+        break;
+    case '11':
+        console.log('Осень');
+        break;
+    case '12':
+        console.log('Зима');
+        break;
+    default:
+        console.log('Такого месяца не существует');
+        break;
+}
